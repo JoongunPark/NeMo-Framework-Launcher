794fc440627427032d9ad001388a9e52b7bd363c
Ted Jiang <tedj@cs-oci-ord-dc-02.cm.cluster>
2024-05-02 07:28:16 -0700

    updated llama script for chakra

--- examples/llama2_7b_bf16.sh
+++ examples/llama2_7b_bf16.sh
@@ -4,10 +4,10 @@
 NEMO_MEGATRON_LAUNCHER_DIR=${NEMO_MEGATRON_LAUNCHER_DIR:-"/opt/NeMo-Megatron-Launcher"}
 DATA_DIR=${DATA_DIR}
 TEST_NAME="llama2_7b_bf16"
-export CHAKRA_DIRECTORY=${NEMO_MEGATRON_LAUNCHER_DIR}/results/${TEST_NAME}/
+#export CHAKRA_DIRECTORY=${NEMO_MEGATRON_LAUNCHER_DIR}/results/${TEST_NAME}/
 #export CHAKRA_BARRIER="yes"
 #export CHAKRA_CRASH="yes"
-export CHAKRA_STEP="40"
+#export CHAKRA_STEP="40"
 
 HYDRA_FULL_ERROR=1 python3 ${NEMO_MEGATRON_LAUNCHER_DIR}/launcher_scripts/main.py \
 training=llama/llama2_7b \
@@ -27,11 +27,29 @@ training.trainer.val_check_interval=20 \
 training.exp_manager.create_checkpoint_callback=False \
 training.model.chakra_profile.enabled=True \
 training.model.chakra_profile.start_step=2 \
-training.model.chakra_profile.end_step=4 \
+training.model.chakra_profile.end_step=3 \
 training.model.chakra_profile.warmup_steps=1 \
-training.model.chakra_profile.active_steps=2 \
-#cluster_type=interactive \
+training.model.chakra_profile.active_steps=1 \
+
 #training.model.nsys_profile.enabled=True \
+#training.model.nsys_profile.start_step=2 \
+#training.model.nsys_profile.end_step=3 \
+#cluster_type=interactive \
+
+#training.model.chakra_profile.enabled=True \
+#training.model.chakra_profile.start_step=2 \
+#training.model.chakra_profile.end_step=3 \
+#training.model.chakra_profile.warmup_steps=1 \
+#training.model.chakra_profile.active_steps=1 \
+
+
+
 #training.model.tensor_model_parallel_size=2 \
 #training.model.pipeline_model_parallel_size=2 \
 #cluster_type=interactive \
+
+#training.model.chakra_profile.enabled=True \
+#training.model.chakra_profile.start_step=2 \
+#training.model.chakra_profile.end_step=4 \
+#training.model.chakra_profile.warmup_steps=1 \
+#training.model.chakra_profile.active_steps=2 \
